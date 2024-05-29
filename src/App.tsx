33bbea7bc7f96a408a28b8556f93b4a9e15a5f14
JJLorente <joel.jimenez.lorente@gmail.com>
2024-05-29 23:45:47 +0200

    Bug fixed bg home

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ function App() {
 
   return (
     <div className='App'>
-
+      
       <div className='container-app'>
 
         <div className='container-title'>
           <h1 className="title jaro-regular">
             GACHANIME
           </h1>
+          <img className='goku-up-img' src="./goku-sticker-1.png" alt="sticker goku" />
         </div>
 
         <div className='container-form'>
           <img className='naruto-up-img' src="./naruto-sticker-1.png" alt="sticker naruto" />
-          <img className='goku-up-img' src="./goku-sticker-1.png" alt="sticker goku" />
         </div>
 
       </div>
