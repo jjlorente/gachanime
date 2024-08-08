ef635659d7c94578f94d997babcc9dba5dce012d
JJLorente <joel.jimenez.lorente@gmail.com>
2024-08-08 21:48:44 +0200

    Test console log production

--- src/services/userGames.tsx
+++ src/services/userGames.tsx
@@ -169,7 +169,7 @@ export const deleteAll = async () => {
             const errorData = await response.json();
             throw new Error(errorData.error || 'Unknown error');
         }
-
+        console.log(response)
         const data = await response.json();
         return data;
     } catch (error) {
