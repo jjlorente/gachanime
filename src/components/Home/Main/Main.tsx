332e0df7b6197c76c775db32bdde9a61638d333b
JJLorente <joel.jimenez.lorente@gmail.com>
2024-08-13 22:58:12 +0200

    SubSections added

--- src/components/Home/Main/Main.tsx
+++ src/components/Home/Main/Main.tsx
@@ -3,7 +3,15 @@ import './Main.css'
 export const Main = (props:any) => {
   return (
     <div className="Main">
-      <div className='section-main'>HOME</div>
+      <div className='section-main'>
+        <div style={{display:"flex", flexDirection:"row", gap:"2rem"}}>
+          <div className='subsection-main'>NOTICIAS</div>
+          <div className='subsection-main'>PARCHES</div>
+        </div>
+        <div className='subsection-main'>
+          ENCUESTA
+        </div>
+      </div>
     </div>
   )
 }
