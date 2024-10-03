5a0b7c9793d3195ea2fe0461895f6df1ea60f3e2
JJLorente <joel.jimenez.lorente@gmail.com>
2024-10-03 18:10:21 +0200

    Test on railway

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   googleClientId: "343896712510-niddt5vhrnapb2gep298evcio2m9jtd4.apps.googleusercontent.com",
-  apiUrl: "gachanime-backend-production.up.railway.app/api"
+  apiUrl: "https://gachanime-backend-production.up.railway.app/api"
   // apiUrl: "http://localhost:3000/api"
 };
 
