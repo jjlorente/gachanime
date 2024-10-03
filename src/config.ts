20bd6a469bfe39b1af438366c8496cc1d9d5403c
JJLorente <joel.jimenez.lorente@gmail.com>
2024-10-03 14:15:49 +0200

    Reset fixed

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 const config = {
   googleClientId: "343896712510-niddt5vhrnapb2gep298evcio2m9jtd4.apps.googleusercontent.com",
-  // apiUrl: "https://gachanime-backend.onrender.com/api"
-  apiUrl: "http://localhost:3000/api"
+  apiUrl: "https://gachanime-backend.onrender.com/api"
 };
 
 export default config;
